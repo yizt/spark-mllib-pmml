c717a0553d02b43ab548490d6f5dce6764df854b
mick.yi <mick.yi@embracesource.com>
2018-09-13 17:02:40 +0800

    完善命令行参数

--- src/main/scala/com/es/analyze/train/TrainMain.scala
+++ src/main/scala/com/es/analyze/train/TrainMain.scala
@@ -23,12 +23,17 @@ object TrainMain {
                     appName: String = "TrainMain"
                    )
 
-  val parser = new OptionParser[Params]("Train Spark Model") {
+  val parser = new OptionParser[Params]("Train_Spark_Model") {
     head("Train Spark Model:.")
     opt[String]('m', "model")
       .required()
       .text("模型名称")
       .action((x, c) => c.copy(modelName = x))
+      .validate(x => {
+        if ("DTCls,DTRgr,RFCls,RFRgr,GBTCls,GBTRgr,XGBCls,XGBRgr".contains(x)) success
+        else failure("Option --模型名称必须是DTCls,DTRgr,RFCls,RFRgr,GBTCls,GBTRgr,XGBCls,XGBRgr中一个")
+      })
+    //输入数据
     opt[String]('t', "tablename")
       .required()
       .text("表名")
@@ -41,15 +46,40 @@ object TrainMain {
       .required()
       .text("目标列")
       .action((x, c) => c.copy(targetCol = x))
-    opt[String]("appName")
-      .required()
-      .text("appName")
-      .action((x, c) => c.copy(appName = x))
-    opt[Map[String, String]]("kwargs").valueName("k1=v1,k2=v2...").
-      action((x, c) => c.copy(kwargs = x)).text("其它参数")
-    opt[Seq[Double]]("splitRatio").valueName("<trainweight,testweight>")
+    opt[String]("filters")
+      .optional()
+      .text("过滤条件")
+      .action((x, c) => c.copy(filters = x))
+
+    //训练参数
+    opt[Seq[Double]]("splitRatio")
+      .valueName("<trainweight,testweight>")
       .text("分割比例,如:3,1")
       .action((x, c) => c.copy(splitRatio = x))
+
+    //输出参数
+    opt[String]("pmml")
+      .optional()
+      .text("pmml文件输出路径")
+      .action((x, c) => c.copy(outputPmml = x))
+    opt[String]('s', "modelSavePath")
+      .required()
+      .text("模型保存路径")
+      .action((x, c) => c.copy(modelSavePath = x))
+
+
+    //交叉验证
+    opt[Unit]("crossvalid").action((_, c) =>
+      c.copy(crossValid = true)).text("crossvalid是标志参数,代表启用交叉验证")
+    opt[Int]('k', "folds")
+      .required()
+      .text("k折交叉验证")
+      .action((x, c) => c.copy(folds = x))
+
+    //其它参数
+    opt[Map[String, String]]("kwargs")
+      .valueName("k1=v1,k2=v2...").
+      action((x, c) => c.copy(kwargs = x)).text("其它参数")
 
   }
 
